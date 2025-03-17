075331cac3f800c20082f4ac72860c1936666211
lorussogiacomoluca <lorusso6u90@gmail.com>
2025-03-17 16:34:11 +0100

    [UPD] pricePerKm variable

---
+++ js/script.js
@@ -0,0 +1,14 @@
+/*
+SOTTOPROBLEMI
+- creazione variabile costante: pricePerKm
+- creazione variabile costante: discount18
+- creazione variabile costante: discount65
+- dichiarazione e assegnazione tramite prompt della variabile km
+- dichiarazione e assegnazione tramite prompt della variabile age
+- blocco IF per individuare sconto da applicare in base ad età
+- calcolo del prezzo finale
+- formmattazione del prezzo finale con due decimali
+*/
+
+// pricePerKm variable
+let pricePerKm = 0.21;
